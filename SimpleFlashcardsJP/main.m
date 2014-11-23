ff99c4ff0ae7f5f41f6e7a67df55db5415b94d56
Teru <linzhaoxing@hotmail.co.jp>
2014-11-23 17:32:51 +0900

    Initial Commit

---
+++ SimpleFlashcardsJP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SimpleFlashcardsJP
+//
+//  Created by Teru on 2014/11/23.
+//  Copyright (c) 2014年 Self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
